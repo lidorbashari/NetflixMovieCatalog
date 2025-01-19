057d6ff08c73774fdb270250a8429c9c36ef053e
lidor <lidor1965@gmail.com>
2025-01-19 16:06:01 +0200

    6 try

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with open('data/data_movies.json', 'r') as f:
 
 @app.route("/", methods=['GET'])
 def home():
-    return "Hi! This app is an API, there is no UI ;-) lidor bli alon"
+    return "Hi! This app is an API, there is no UI ;-) lidor bashari"
 
 
 @app.route('/discover')
